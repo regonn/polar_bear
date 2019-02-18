86427ebd75abf06b560b36f958169f463cc2af75
regonn <programmer@regonn.biz>
2019-02-18 12:47:12 +0900

    remove same value columns

--- polar_bear/main.py
+++ polar_bear/main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 
-int_dtype_list = ['int8', 'int16', 'int32', 'int64', 'uint8', 'uint16', 'uint32', 'uint64']
+int_dtype_list = ['int8', 'int16', 'int32',
+                  'int64', 'uint8', 'uint16', 'uint32', 'uint64']
 float_dtype_list = ['float16', 'float32', 'float64', 'float128']
 
 
@@ -19,37 +20,44 @@ def clean(train_df, test_df, target_col, threshold_one_hot=0.1):
         content = pd.concat([content, test_df[label]])
         dtype = content.dtype
 
+        value_counts = content.value_counts()
+        value_counts_number = value_counts.shape[0]
+
+        if value_counts_number == 1:
+            continue
+
         if dtype in int_dtype_list:
-            value_counts = content.value_counts().shape[0]
-            if value_counts < (rows_count * threshold_one_hot):
-                mode = content.value_counts().index[0]
-                content[np.isnan(content)] = mode
+            if value_counts_number < (rows_count * threshold_one_hot):
+                mode_value = value_counts.index[0]
+                content[np.isnan(content)] = mode_value
                 one_hot_df = pd.get_dummies(content, prefix=label)
                 for one_hot_label, one_hot_content in one_hot_df.iteritems():
-                    return_df["x" + str(feature_column_index) + ":" + one_hot_label] = one_hot_content
+                    return_df["x" + str(feature_column_index) +
+                              ":" + one_hot_label] = one_hot_content
                     feature_column_index += 1
         elif dtype in float_dtype_list:
-            value_counts = content.value_counts().shape[0]
-            if value_counts < (rows_count * threshold_one_hot):
-                mode = content.value_counts().index[0]
-                content[np.isnan(content)] = mode
+            if value_counts_number < (rows_count * threshold_one_hot):
+                mode_value = content.value_counts().index[0]
+                content[np.isnan(content)] = mode_value
                 one_hot_df = pd.get_dummies(content, prefix=label)
                 for one_hot_label, one_hot_content in one_hot_df.iteritems():
-                    return_df["x" + str(feature_column_index) + ":" + one_hot_label] = one_hot_content
+                    return_df["x" + str(feature_column_index) +
+                              ":" + one_hot_label] = one_hot_content
                     feature_column_index += 1
             else:
                 mean = content.mean()
                 content[np.isnan(content)] = mean
-                return_df["x" + str(feature_column_index) + ":" + label + "_float"] = content
+                return_df["x" + str(feature_column_index) +
+                          ":" + label + "_float"] = content
                 feature_column_index += 1
         elif (dtype == 'object') or (dtype == 'bool'):
-            value_counts = content.value_counts().shape[0]
-            if value_counts < (rows_count * threshold_one_hot):
-                mode = content.value_counts().index[0]
-                content[pd.isnull(content)] = mode
+            if value_counts_number < (rows_count * threshold_one_hot):
+                mode_value = content.value_counts().index[0]
+                content[pd.isnull(content)] = mode_value
                 one_hot_df = pd.get_dummies(content, prefix=label)
                 for one_hot_label, one_hot_content in one_hot_df.iteritems():
-                    return_df["x" + str(feature_column_index) + ":" + one_hot_label] = one_hot_content
+                    return_df["x" + str(feature_column_index) +
+                              ":" + one_hot_label] = one_hot_content
                     feature_column_index += 1
 
     return return_df[0:len(train_df)], target_df, return_df[len(train_df):]
--- tests/test_polar_bear.py
+++ tests/test_polar_bear.py
@@ -7,16 +7,17 @@ import pandas as pd
 
 
 class TestPolarBear(TestCase):
-    def test_say(self):
+    def test_clean(self):
         train_df = pd.read_csv('tests/train.csv')
         test_df = pd.read_csv('tests/test.csv')
         target_col = 'target'
-        cleaned_train_df, target_df, cleaned_test_df = pb.clean(train_df, test_df, target_col, 0.5)
+        cleaned_train_df, target_df, cleaned_test_df = pb.clean(
+            train_df, test_df, target_col, 0.5)
         print(cleaned_train_df)
         print(cleaned_test_df)
         print(target_df)
         self.assertEqual(len(cleaned_test_df.dropna()), len(test_df))
         self.assertEqual(len(cleaned_train_df), len(target_df))
-        self.assertEqual(len(cleaned_train_df.columns), len(cleaned_test_df.columns))
+        self.assertEqual(len(cleaned_train_df.columns),
+                         len(cleaned_test_df.columns))
         self.assertEqual(target_df.columns[0], "y1:" + target_col)
-
