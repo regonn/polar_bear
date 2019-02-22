6b76b498b29938083170a7d39fe724a81f6ac237
regonn <programmer@regonn.biz>
2019-02-22 17:45:09 +0900

    0.0.5 release!!

--- polar_bear/__init__.py
+++ polar_bear/__init__.py
@@ -1,10 +1,10 @@
 from .main import *
 
-__copyright__    = 'Copyright (C) 2019 regonn'
-__version__      = '0.0.4'
-__license__      = 'MIT'
-__author__       = 'regonn'
+__copyright__ = 'Copyright (C) 2019 regonn'
+__version__ = '0.0.5'
+__license__ = 'MIT'
+__author__ = 'regonn'
 __author_email__ = 'regonn@haizine.art'
-__url__          = 'https://github.com/regonn/polar_bear'
+__url__ = 'https://github.com/regonn/polar_bear'
 
 __all__ = ['main']
--- polar_bear/main.py
+++ polar_bear/main.py
@@ -1,5 +1,10 @@
 import numpy as np
 import pandas as pd
+import optuna
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 int_dtype_list = ['int8', 'int16', 'int32',
                   'int64', 'uint8', 'uint16', 'uint32', 'uint64']
@@ -35,13 +40,10 @@ def _target_data(train_df: pd.DataFrame, target_col: str) -> pd.DataFrame:
     return target_df
 
 
-def clean(train_df, test_df, target_col, threshold_one_hot=0.1):
+def _make_return_df(train_df, test_df, threshold_one_hot):
     return_df = pd.DataFrame()
     rows_count = len(train_df) + len(test_df)
     feature_column_index = 1
-
-    target_df = _target_data(train_df, target_col)
-    del train_df[target_col]
 
     for label, content in train_df.iteritems():
         content = pd.concat([content, test_df[label]])
@@ -86,5 +88,43 @@ def clean(train_df, test_df, target_col, threshold_one_hot=0.1):
                     return_df["x" + str(feature_column_index) +
                               ":" + one_hot_label] = one_hot_content
                     feature_column_index += 1
+
+    return return_df
+
+
+def _wrapper_objective(train_df, target_df, test_df):
+    target_dtype = target_df.dtypes[0]
+    if target_dtype in float_dtype_list:
+        if target_df.iloc[:, 0].value_counts().shape[0] < 10:
+            rf = RandomForestClassifier()
+        else:
+            rf = RandomForestRegressor()
+    else:
+        rf = RandomForestClassifier()
+
+    def objective(trial):
+        threshold_one_hot = trial.suggest_discrete_uniform(
+            'threshold_one_hot', 0.0, 1.0, 0.01)
+        return_df = _make_return_df(train_df, test_df, threshold_one_hot)
+        X_train, X_test, y_train, y_test = train_test_split(
+            return_df[0:len(train_df)].values, target_df.iloc[:, 0].values, test_size=0.2, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_test)
+        print(y_pred)
+        return 1.0 - accuracy_score(y_test, y_pred)
+    return objective
+
+
+def clean(train_df, test_df, target_col, threshold_one_hot=None):
+    target_df = _target_data(train_df, target_col)
+    del train_df[target_col]
+
+    if threshold_one_hot is None:
+        study = optuna.create_study()
+        study.optimize(_wrapper_objective(train_df, target_df, test_df), 100)
+        return_df = _make_return_df(
+            train_df, test_df, study.best_params['threshold_one_hot'])
+    else:
+        return_df = _make_return_df(train_df, test_df, threshold_one_hot)
 
     return return_df[0:len(train_df)], target_df, return_df[len(train_df):]
