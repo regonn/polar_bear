6ec96a4756207d49bc76236dbfac6bc37f5fba22
regonn <programmer@regonn.biz>
2019-06-12 23:27:26 +0900

    add expected csv

--- tests/test_polar_bear.py
+++ tests/test_polar_bear.py
@@ -13,14 +13,19 @@ class TestPolarBear(TestCase):
         target_col = 'target'
         cleaned_train_df, target_df, cleaned_test_df = pb.clean(
             train_df, test_df, target_col, 0.5)
-        print(cleaned_train_df)
-        print(cleaned_test_df)
-        print(target_df)
+
         self.assertEqual(len(cleaned_test_df.dropna()), len(test_df))
         self.assertEqual(len(cleaned_train_df), len(target_df))
         self.assertEqual(len(cleaned_train_df.columns),
                          len(cleaned_test_df.columns))
         self.assertEqual(target_df.columns[0], "y1:" + target_col)
+
+        expected_train_df = pd.read_csv('tests/expected_train.csv')
+        expected_test_df = pd.read_csv('tests/expected_test.csv')
+        print(cleaned_train_df.columns.to_list())
+        print(expected_train_df.columns.to_list())
+        self.assertListEqual(cleaned_train_df.columns.to_list(), expected_train_df.columns.to_list())
+        self.assertListEqual(cleaned_test_df.columns.to_list(), expected_test_df.columns.to_list())
 
     def test_clean_optuna(self):
         train_df = pd.read_csv('tests/train.csv')
@@ -28,9 +33,6 @@ class TestPolarBear(TestCase):
         target_col = 'target'
         cleaned_train_df, target_df, cleaned_test_df = pb.clean(
             train_df, test_df, target_col)
-        print(cleaned_train_df)
-        print(cleaned_test_df)
-        print(target_df)
         self.assertEqual(len(cleaned_test_df.dropna()), len(test_df))
         self.assertEqual(len(cleaned_train_df), len(target_df))
         self.assertEqual(len(cleaned_train_df.columns),
