a981cf3f6637e3ccce9268e77b06aed90f2fb373
regonn <programmer@regonn.biz>
2019-06-13 10:57:34 +0900

    version up 0.0.10

--- polar_bear/__init__.py
+++ polar_bear/__init__.py
@@ -3,7 +3,6 @@ from polar_bear.ludwig import *
 from polar_bear.version import __version__
 
 __copyright__ = 'Copyright (C) 2019 regonn'
-__version__ = '0.0.9'
 __license__ = 'MIT'
 __author__ = 'regonn'
 __author_email__ = 'regonn@haizine.art'
