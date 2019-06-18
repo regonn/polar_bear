764d52dfbb40f17133030296f2e5a1bd0eb6b884
regonn <programmer@regonn.biz>
2019-06-18 12:39:32 +0900

    multi_category test

--- polar_bear/main.py
+++ polar_bear/main.py
@@ -14,22 +14,34 @@ float_dtype_list = ['float16', 'float32', 'float64']
 def convert_multi_category(train_df, test_df, split=','):
     df = pd.concat([train_df, test_df])
 
+    splited_values = df.apply(
+        pd.value_counts).index.str.split(split).to_series()
 
-    columns = set(list(chain.from_iterable(df.apply(pd.value_counts).index.str.split(split).to_list())))
+    striped_values = pd.Series(
+        list(chain.from_iterable(splited_values.to_list()))).str.strip()
+
+    columns = set(striped_values)
 
     column_names = list(map(lambda x: 'label_' + x, columns))
     return_df = pd.DataFrame(columns=column_names)
-    print(column_names)
 
     for _, row in df.iterrows():
-        unique_values = list(chain.from_iterable(pd.Series(row).dropna().str.split(split).to_list()))
+        droped_values = list(chain.from_iterable(
+            pd.Series(row).dropna().str.split(split).to_list()))
+        if len(droped_values) == 0:
+            unique_values = []
+        else:
+            unique_values = pd.Series(droped_values).str.strip().values
         row_df = pd.DataFrame()
         for column in columns:
-            row_df['label_' + column] = [1] if (column in unique_values) else [0]
-        return_df = return_df.append(row_df)
+            row_df['label_' +
+                   column] = [1] if (column in unique_values) else [0]
+        return_df = return_df.append(row_df, ignore_index=True)
 
+    return_train_df = return_df[0:len(train_df)]
+    return_test_df = return_df[len(train_df):].reset_index()
 
-    return return_df[0:len(train_df)], return_df[len(train_df):]
+    return return_train_df, return_test_df
 
 
 def _target_data(train_df: pd.DataFrame, target_col: str) -> pd.Series:
@@ -103,13 +115,21 @@ def convert_series(train_series: pd.Series, test_series: pd.Series, threshold_on
     return return_df[0:len(train_series)], return_df[len(train_series):]
 
 
-def _make_return_df(train_df, test_df, target_col, threshold_one_hot):
+def _make_return_df(train_df, test_df, target_col, threshold_one_hot, multi_category):
+    if (multi_category is None):
+        multi_category_columns = []
+    else:
+        multi_category_columns = list(chain.from_iterable(multi_category))
+
     return_train_df = pd.DataFrame()
     return_test_df = pd.DataFrame()
     feature_column_index = 1
 
     for label, train_series in train_df.iteritems():
         if (label == target_col):
+            continue
+
+        if (label in multi_category_columns):
             continue
 
         value_counts = train_series.value_counts()
@@ -127,11 +147,21 @@ def _make_return_df(train_df, test_df, target_col, threshold_one_hot):
             return_train_df[label_name] = converted_train_content
             return_test_df[label_name] = converted_test_df[converted_label]
             feature_column_index += 1
+    if (multi_category is not None):
+        for multi_columns in multi_category:
+            converted_train_df, converted_test_df = convert_multi_category(
+                train_df[multi_columns], test_df[multi_columns])
+            for converted_label, converted_train_content in converted_train_df.iteritems():
+                label_name = "x" + str(feature_column_index) + \
+                    ":" + converted_label
+                return_train_df[label_name] = converted_train_content
+                return_test_df[label_name] = converted_test_df[converted_label]
+                feature_column_index += 1
 
     return return_train_df, return_test_df
 
 
-def _wrapper_objective(train_df, test_df, target_series, target_col):
+def _wrapper_objective(train_df, test_df, target_series, target_col, multi_category):
     target_dtype = target_series.dtype
     n_estimators = 10
     if target_dtype in float_dtype_list:
@@ -149,7 +179,7 @@ def _wrapper_objective(train_df, test_df, target_series, target_col):
         threshold_one_hot = trial.suggest_int(
             'threshold_one_hot', 0, 100) * 0.01
         return_train_df, return_test_df = _make_return_df(
-            train_df, test_df, target_col, threshold_one_hot)
+            train_df, test_df, target_col, threshold_one_hot, multi_category)
         X_train, X_test, y_train, y_test = train_test_split(
             return_train_df, target_series.values, test_size=0.2, random_state=0)
         rf.fit(X_train, y_train)
@@ -161,17 +191,17 @@ def _wrapper_objective(train_df, test_df, target_series, target_col):
     return objective
 
 
-def clean(train_df, test_df, target_col, threshold_one_hot=None):
+def clean(train_df, test_df, target_col, threshold_one_hot=None, multi_category=None):
     target_series = _target_data(train_df, target_col)
 
     if threshold_one_hot is None:
         study = optuna.create_study()
         study.optimize(_wrapper_objective(
-            train_df, test_df, target_series, target_col), n_trials=100, timeout=10 * 60)
+            train_df, test_df, target_series, target_col, multi_category), n_trials=100, timeout=10 * 60)
         return_train_df, return_test_df = _make_return_df(
-            train_df, test_df, target_col, study.best_params['threshold_one_hot'] * 0.01)
+            train_df, test_df, target_col, study.best_params['threshold_one_hot'] * 0.01, multi_category)
     else:
         return_train_df, return_test_df = _make_return_df(
-            train_df, test_df, target_col, threshold_one_hot)
+            train_df, test_df, target_col, threshold_one_hot, multi_category)
 
     return return_train_df, target_series, return_test_df
--- tests/test_polar_bear.py
+++ tests/test_polar_bear.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 
 class TestPolarBear(unittest.TestCase):
+    maxDiff = None
+
     def test_convert_multi_category(self):
         train_df = pd.read_csv('tests/train.csv')
         test_df = pd.read_csv('tests/test.csv')
@@ -14,7 +16,8 @@ class TestPolarBear(unittest.TestCase):
         target_train_df = train_df[target_columns]
         target_test_df = test_df[target_columns]
 
-        converted_multi_train_df, converted_multi_test_df = pb.convert_multi_category(target_train_df, target_test_df)
+        converted_multi_train_df, converted_multi_test_df = pb.convert_multi_category(
+            target_train_df, target_test_df)
         print(converted_multi_train_df)
         print(converted_multi_test_df)
 
@@ -22,17 +25,26 @@ class TestPolarBear(unittest.TestCase):
         train_df = pd.read_csv('tests/train.csv')
         test_df = pd.read_csv('tests/test.csv')
         target_column = 'class_text'
-        converted_train_df, converted_test_df = pb.convert_series(train_df[target_column], test_df[target_column])
-        expected_converted_train_df = pd.read_csv('expected_converted_train.csv')
+        converted_train_df, converted_test_df = pb.convert_series(
+            train_df[target_column], test_df[target_column])
+        expected_converted_train_df = pd.read_csv(
+            'expected_converted_train.csv')
         expected_converted_test_df = pd.read_csv('expected_converted_test.csv')
-        self.assertListEqual(converted_train_df.columns.to_list(), expected_converted_train_df.columns.to_list())
-        self.assertListEqual(converted_test_df.columns.to_list(), expected_converted_test_df.columns.to_list())
+        self.assertListEqual(converted_train_df.columns.to_list(
+        ), expected_converted_train_df.columns.to_list())
+        self.assertListEqual(converted_test_df.columns.to_list(
+        ), expected_converted_test_df.columns.to_list())
 
-        converted_train_with_nan_df, converted_test_with_nan_df = pb.convert_series(train_df[target_column], test_df[target_column], include_dummy_na=True)
-        expected_converted_train_with_nan_df = pd.read_csv('expected_converted_with_nan_train.csv')
-        expected_converted_test_with_nan_df = pd.read_csv('expected_converted_with_nan_test.csv')
-        self.assertListEqual(converted_train_with_nan_df.columns.to_list(), expected_converted_train_with_nan_df.columns.to_list())
-        self.assertListEqual(converted_test_with_nan_df.columns.to_list(), expected_converted_test_with_nan_df.columns.to_list())
+        converted_train_with_nan_df, converted_test_with_nan_df = pb.convert_series(
+            train_df[target_column], test_df[target_column], include_dummy_na=True)
+        expected_converted_train_with_nan_df = pd.read_csv(
+            'expected_converted_with_nan_train.csv')
+        expected_converted_test_with_nan_df = pd.read_csv(
+            'expected_converted_with_nan_test.csv')
+        self.assertListEqual(converted_train_with_nan_df.columns.to_list(
+        ), expected_converted_train_with_nan_df.columns.to_list())
+        self.assertListEqual(converted_test_with_nan_df.columns.to_list(
+        ), expected_converted_test_with_nan_df.columns.to_list())
 
     def test_clean(self):
         train_df = pd.read_csv('tests/train.csv')
@@ -52,8 +64,49 @@ class TestPolarBear(unittest.TestCase):
         self.assertListEqual(
             new_train_df.columns.to_list(), train_df.columns.to_list())
 
+        # Update Expected CSV
+        cleaned_train_df.to_csv('tests/expected_train.csv', index=False)
+        cleaned_test_df.to_csv('tests/expected_test.csv', index=False)
+
         expected_train_df = pd.read_csv('tests/expected_train.csv')
         expected_test_df = pd.read_csv('tests/expected_test.csv')
+        self.assertListEqual(cleaned_train_df.columns.to_list(),
+                             expected_train_df.columns.to_list())
+        self.assertListEqual(cleaned_test_df.columns.to_list(),
+                             expected_test_df.columns.to_list())
+
+    def test_clean_multi_category(self):
+        train_df = pd.read_csv('tests/train.csv')
+        test_df = pd.read_csv('tests/test.csv')
+        target_col = 'target'
+        multi_categories = [['multi_label1', 'multi_label2',
+                             'multi_label3'], ['multi_label_independed']]
+        cleaned_train_df, target_series, cleaned_test_df = pb.clean(
+            train_df, test_df, target_col, 0.5, multi_categories)
+
+        print(cleaned_test_df)
+
+        self.assertEqual(len(cleaned_test_df.dropna()), len(test_df))
+        self.assertEqual(len(cleaned_train_df), len(target_series))
+        self.assertEqual(len(cleaned_train_df.columns),
+                         len(cleaned_test_df.columns))
+        self.assertEqual(target_series.name, "y1:" + target_col)
+
+        # Check no effect for original dataframe
+        new_train_df = pd.read_csv('tests/train.csv')
+        self.assertListEqual(
+            new_train_df.columns.to_list(), train_df.columns.to_list())
+
+        # Update Expected CSV
+        cleaned_train_df.to_csv(
+            'tests/expected_multi_category_train.csv', index=False)
+        cleaned_test_df.to_csv(
+            'tests/expected_multi_category_test.csv', index=False)
+
+        expected_train_df = pd.read_csv(
+            'tests/expected_multi_category_train.csv')
+        expected_test_df = pd.read_csv(
+            'tests/expected_multi_category_test.csv')
         self.assertListEqual(cleaned_train_df.columns.to_list(),
                              expected_train_df.columns.to_list())
         self.assertListEqual(cleaned_test_df.columns.to_list(),
@@ -88,8 +141,22 @@ class TestPolarBear(unittest.TestCase):
 
         model_definition = pb.make_model_definition_for_ludwig(
             train_df, 'target')
-        self.assertEqual(model_definition, {'output_features': [{'name': 'target', 'type': 'binary'}], 'input_features': [{'name': 'height', 'type': 'numerical'}, {
-                         'name': 'class', 'type': 'numerical'}, {'name': 'class_text', 'type': 'category'}, {'name': 'switch', 'type': 'category'}]})
+        expected_model_definition = {
+            'output_features':
+            [{'name': 'target', 'type': 'binary'}],
+            'input_features':
+            [
+                {'name': 'height', 'type': 'numerical'},
+                {'name': 'class', 'type': 'numerical'},
+                {'name': 'class_text', 'type': 'category'},
+                {'name': 'switch', 'type': 'category'},
+                {'name': 'multi_label1', 'type': 'category'},
+                {'name': 'multi_label2', 'type': 'category'},
+                {'name': 'multi_label3', 'type': 'category'}
+            ]
+        }
+        self.assertDictEqual(model_definition, expected_model_definition)
+
 
 if __name__ == '__main__':
     unittest.main()
